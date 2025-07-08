0cb54a58169573847019da5f5b2f1effc6195587
ayman-othman <aymanyori00@gmail.com>
2025-07-08 10:06:01 +0300

    bigInt to string

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -9,13 +9,23 @@ export class ReviewService {
     return this.contractService.addReview(dto.productId, dto.comment, dto.rating);
   }
 
-  async getAllReviews() {
-    const count = await this.contractService.getReviewCount();
-    const reviews: any[] = [];
-    for (let i = 0; i < count; i++) {
-      const review = await this.contractService.getReview(i);
-      reviews.push(review);
-    }
-    return reviews;
+async getAllReviews() {
+  const count = await this.contractService.getReviewCount();
+  const reviews = [];
+
+  for (let i = 0; i < count; i++) {
+    const review = await this.contractService.getReview(i);
+
+    const normalizedReview = {
+      productId: review[0],
+      comment: review[1],
+      rating: review[2].toString(),
+    };
+
+    reviews.push(normalizedReview);
   }
+
+  return reviews;
+}
+
 }
